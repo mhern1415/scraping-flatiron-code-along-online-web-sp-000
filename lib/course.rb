1a5711f0994f4472b13a921aa96656c6ac615956
Mark Hernandez <m.hern1415@gmail.com>
2019-06-10 00:23:24 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,17 @@
 class Course
 attr_accessor :title, :schedule, :description
 
+def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
 
 end
 
